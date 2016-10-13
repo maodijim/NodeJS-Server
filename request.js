122ed6d2a0fef8885a545719ad3c7c9e19aff946
Andy Mao <dmao1@mail.sfsu.edu>
2016-10-13 01:28:47 -0700

    v1.0.9
    fix

--- request.js
+++ request.js
@@ -66,8 +66,10 @@ var myfunction = function(){
       if (!err && res.statusCode === 200) {
         if(body == 'match'){
         }else if (body == 'update') {
-          execSync('git reset --hard');
           execSync('git pull');
+          execSync('sudo pm2 restart bin/www');
+          execSync('sudo pm2 restart request.js');
+          execSync('sudo chmod +x codesend RFSniffer1');
         }else{
           var json = JSON.parse(body);
           if(data.devices.length == json.length){
