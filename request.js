2f8f78a6b0ff52fc222fc12aeea01b5a9479f643
Andy Mao <dmao1@mail.sfsu.edu>
2016-10-18 15:11:01 -0700

    v1.1.1
    fix

--- request.js
+++ request.js
@@ -69,6 +69,8 @@ var myfunction = function(){
           execSync('git checkout .');
           execSync('git pull');
           execSync('sudo chmod +x codesend RFSniffer1');
+          execSync('sudo pm2 restart request.js');
+          execSync('sudo pm2 restart bin/www');
         }else if (body.substr(0,5) == 'order') {
           //Device Order Change Handler
           var arr = body.split(/[:,]/);
