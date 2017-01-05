05fc9737dfcaf61e4d89f53b83b821e98e7e8b87
andymao <dmao1@mail.sfsu.edu>
2017-01-05 17:02:41 +0800

    v1.1.9 bug fixes

--- wifiCheck.py
+++ wifiCheck.py
@@ -6,13 +6,13 @@ time = datetime.datetime.now()
 apMode = subprocess.check_output("ping 10.0.0.1 -c 1 -W 1 | grep '1 received' | wc -l", shell=True)
 
 def reconnectmqtt():
-    subprocess.call(["sudo", "pm2", "restart", "mqtt"])
-
+    subprocess.call(["sudo", "pm2", "delete", "mqtt"])
+    subprocess.call(["sudo", "pm2", "start", "/home/pi/Public/NodeJS-Server/mqtt.js"])
 def test():
     try:
         if('from 8.8.8.8' in (subprocess.check_output("ping 8.8.8.8 -c 4 -W 15 | grep '8.8.8.8'", shell=True))):
             print('Reconnect to Internet')
-            threading.Timer(15,reconnectmqtt).start()
+            '''threading.Timer(15,reconnectmqtt).start()'''
     except subprocess.CalledProcessError as e:
         subprocess.call(['sudo','ifdown','wlan0'])
         subprocess.call(['sudo','cp','/etc/network/interfaces2','/etc/network/interfaces'])
