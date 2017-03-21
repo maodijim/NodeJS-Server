a6789596e515500377beb4796882bbb7de16ae21
Andy Mao <dmao1@mail.sfsu.edu>
2017-03-21 01:12:26 -0700

    Delete bucky.js

--- bucky.js
+++
@@ -1,9 +0,0 @@
-var movies = require('././movies');
-
-var buckyMovie = movies();
-buckyMovie.favMovie = "Good Will Hunting";
-console.log("Bucky's favorite movie is: " + buckyMovie.favMovie);
-
-setInterval(function(){
-  console.log("hello")},1000
-);
