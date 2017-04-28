7abaf9ae2de07f314d7c0dd3e747caf0e2ff9978
andymao <dmao1@mail.sfsu.edu>
2017-04-28 03:38:58 -0700

    v1.2.2 new mqtt

--- mqtt.js
+++ mqtt.js
@@ -14,16 +14,16 @@ connection.query("SELECT * FROM id;",function(err,rows,fields){
   if(err) throw err;
   var id = crypto.createHash('sha1').update(rows[0].id).digest('hex');
   //const mqtt = spawn('mosquitto_sub',['-h','52.201.197.194','-t',id,'-v']);
-  client.on('connect',function(){
+  client.on('connect', function () {
     client.subscribe(id)
   })
 
-  client.on('message',(topic,message)=>{
+  client.on('message',function(topic,message){
     var result = message.toString().split(" ");
-    if(result[1].includes("change")){
+    if(result[0].includes("change")){
       statusChange.update();
-    }else if (result[1].includes("time:")) {
-        var schedule = result[1].substring(5).split(/-|\n/);
+    }else if (result[0].includes("time:")) {
+        var schedule = result[0].substring(5).split(/-|\n/);
         var status = schedule[1];
         var deviceNum = schedule[0];
         if (schedule[3] == "every"){
